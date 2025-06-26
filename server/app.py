2ef98d82c38499611d178a42708957e0641a4cf2
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-06-26 13:30:28 +0300

    replaced to_dict with marshmallow schema for HATEOAS-compliant serialization

--- server/app.py
+++ server/app.py
@@ -15,74 +15,76 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+ma = Marshmallow(app)
 api = Api(app)
 
-class Index(Resource):
+# -------------------------
+# Marshmallow Schema Setup
+# -------------------------
+class NewsletterSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Newsletter
+        load_instance = True
 
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks({
+        "self": ma.URLFor("newsletterbyid", values=dict(id="<id>")),
+        "collection": ma.URLFor("newsletters"),
+    })
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
+# -------------------------
+# Routes
+# -------------------------
+class Index(Resource):
     def get(self):
-        
-        response_dict = {
-            "index": "Welcome to the Newsletter RESTful API",
-        }
-        
         response = make_response(
-            response_dict,
+            {"index": "Welcome to the Newsletter RESTful API"},
             200,
         )
-
         return response
 
 api.add_resource(Index, '/')
 
 class Newsletters(Resource):
-
     def get(self):
-        
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
-
+        newsletters = Newsletter.query.all()
         response = make_response(
-            response_dict_list,
+            newsletters_schema.dump(newsletters),
             200,
         )
-
         return response
 
     def post(self):
-        
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
         )
-
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
-
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(new_record),
             201,
         )
-
         return response
 
 api.add_resource(Newsletters, '/newsletters')
 
 class NewsletterByID(Resource):
-
     def get(self, id):
-
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
-
+        record = Newsletter.query.filter_by(id=id).first()
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(record),
             200,
         )
-
         return response
 
     def patch(self, id):
-
         record = Newsletter.query.filter_by(id=id).first()
         for attr in request.form:
             setattr(record, attr, request.form[attr])
@@ -90,33 +92,24 @@ class NewsletterByID(Resource):
         db.session.add(record)
         db.session.commit()
 
-        response_dict = record.to_dict()
-
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(record),
             200
         )
-
         return response
 
     def delete(self, id):
-
         record = Newsletter.query.filter_by(id=id).first()
-        
         db.session.delete(record)
         db.session.commit()
 
-        response_dict = {"message": "record successfully deleted"}
-
         response = make_response(
-            response_dict,
+            {"message": "record successfully deleted"},
             200
         )
-
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
